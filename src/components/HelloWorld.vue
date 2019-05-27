952476328b5760dca0a33e3034a087ea0a4948b7
keyi <1120158440@qq.com>
2019-05-27 12:50:54 +0800

    修改helloworld

--- src/components/HelloWorld.vue
+++ src/components/HelloWorld.vue
@@ -8,7 +8,7 @@
 						<p class="information">{{item.name}}</p>
 						<p class="remark">{{item.remark}}</p>
 						<div class="detail">
-							<span @click="User">    ??</span>
+							<span @click="User"> 张三</span>
 							<van-icon name="arrow" />
 						</div>
 					</div>
